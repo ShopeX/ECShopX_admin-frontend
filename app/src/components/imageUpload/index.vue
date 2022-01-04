8c2a8bf1b3886df96a03d090f906614ca8bb4280
zyk <19121152559@163.com>
2022-01-04 18:41:05 +0800

    fix:1

--- app/src/components/imageUpload/index.vue
+++ app/src/components/imageUpload/index.vue
@@ -72,12 +72,7 @@ export default {
     uploadHandle(val) {
       console.log(val)
     },
-<<<<<<< HEAD
-    handleAvatarSuccess(res, file) { 
-=======
     handleAvatarSuccess(res, file) {
-       
->>>>>>> develop
       this.$emit('successHandle', res.data.data)
       this.loading = false
     },

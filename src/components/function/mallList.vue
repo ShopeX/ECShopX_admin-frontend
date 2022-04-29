de153b73430673b27e70c4cc66f40df685456bb4
maqian <maqian@shopex.cn>
2022-04-29 13:40:23 +0800

    ECX-1795

--- src/components/function/mallList.vue
+++ src/components/function/mallList.vue
@@ -731,6 +731,21 @@ export default {
       })
     },
     toggleOpenWechatappLocation (val) {
+      if (this.VERSION_PLATFORM && val == 2) {
+        this.$confirm('关闭后附件商家组件将无法使用', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.updateLocation(val)
+        }).catch(() => {
+          this.is_open_wechatapp_location = 1
+        })
+        return
+      }
+      this.updateLocation(val)
+    },
+    updateLocation (val) {
       let params = {
         is_open_wechatapp_location: val
         // page_type: this.app_page_type[this.app_type]

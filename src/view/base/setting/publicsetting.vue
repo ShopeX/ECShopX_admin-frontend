b9cd6e3f1b702194cd4f03956d70569a853b2bf2
lukaijie <lukaijie@shopex.cn>
2022-05-26 10:58:23 +0800

    fix: 通用设置

--- src/view/base/setting/publicsetting.vue
+++ src/view/base/setting/publicsetting.vue
@@ -5,12 +5,14 @@
       v-model="form"
       size="min"
       :form-list="formList"
+      :submit="false"
     />
-    form.item_page: {{ form.item_page }}
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import { VERSION_STANDARD, VERSION_IN_PURCHASE } from '@/utils'
 export default {
   name: '',
   data () {
@@ -39,6 +41,7 @@ export default {
           label: '分享带门店参数',
           key: 'distributor_param_status',
           type: 'switch',
+          isShow: VERSION_STANDARD,
           tip: `<div><p>
               已开启：
             </p>
@@ -60,69 +63,147 @@ export default {
             <p>
               2.
               顾客通过「管理后台-店铺-店铺商品」生成的商品二维码或其他带店铺参数的二维码进入商城时，其所在店铺将切换为二维码所带参数对应的店铺，同时本地缓存也会切换成参数对应的新店铺。
-            </p></div>`
+            </p></div>`,
+          onChange: async (e) => {
+            const { distributor_param_status } = this.form
+            await this.$api.system.saveShareParams({
+              distributor_param_status
+            })
+          }
         },
         {
           label: '评论设置',
           key: 'rate_status',
-          type: 'switch'
+          type: 'switch',
+          isShow: !VERSION_IN_PURCHASE,
+          onChange: async (e) => {
+            const { rate_status } = this.form
+            await this.$api.company.setRateSetting({
+              rate_status
+            })
+          }
         },
         {
           label: '提货码设置',
           key: 'pickupcode_status',
-          type: 'switch'
+          type: 'switch',
+          isShow: !this.isMicorMall && !VERSION_IN_PURCHASE,
+          onChange: async (e) => {
+            const { pickupcode_status } = this.form
+            await this.$api.company.setPickupcodeSetting({
+              pickupcode_status
+            })
+          }
         },
         {
           label: '会员白名单',
           key: 'whitelist_status',
-          type: 'switch'
+          type: 'switch',
+          onChange: async (e) => {
+            const { whitelist_status } = this.form
+            await this.$api.company.setWhitelistSetting({
+              whitelist_status
+            })
+          }
         },
         {
           label: '赠品设置',
           key: 'check_gift_store',
           type: 'switch',
-          tip: '赠品缺货是否能继续下单'
+          tip: '赠品缺货是否能继续下单',
+          isShow: !VERSION_IN_PURCHASE,
+          onChange: async (e) => {
+            const { check_gift_store } = this.form
+            await this.$api.company.setGiftSetting({
+              check_gift_store
+            })
+          }
         },
         {
           label: '推单设置',
           key: 'ziti_send_oms',
           type: 'switch',
-          tip: '自提订单推oms'
+          tip: '自提订单推oms',
+          isShow: !VERSION_IN_PURCHASE,
+          onChange: async (e) => {
+            const { ziti_send_oms } = this.form
+            await this.$api.company.setSendOmsSetting({
+              ziti_send_oms
+            })
+          }
         },
         {
           label: '前端店铺展示',
           key: 'nostores_status',
           type: 'switch',
-          tip: '用于关闭前端店铺切换功能'
+          tip: '用于开启前端店铺切换功能',
+          isShow: VERSION_STANDARD,
+          onChange: async (e) => {
+            const { nostores_status } = this.form
+            await this.$api.company.setNoStores({
+              nostores_status: !nostores_status
+            })
+          }
         },
         {
           label: '取消订单设置',
           key: 'repeat_cancel',
           type: 'switch',
-          tip: '拒绝后是否能再次发起申请'
+          tip: '拒绝后是否能再次发起申请',
+          onChange: async (e) => {
+            const { repeat_cancel } = this.form
+            await this.$api.company.setRepeatCancelSetting({
+              repeat_cancel
+            })
+          }
         },
         {
           label: '商品库存',
           key: 'item_store_status',
           type: 'switch',
-          tip: '商品详情页是否显示库存'
+          tip: '商品详情页是否显示库存',
+          onChange: async (e) => {
+            const { item_store_status } = this.form
+            await this.$api.company.setItemStoreSetting({
+              item_store_status
+            })
+          }
         },
         {
           label: '商品销量显示',
           key: 'item_sales_status',
-          type: 'switch'
+          type: 'switch',
+          onChange: async (e) => {
+            const { item_sales_status } = this.form
+            await this.$api.company.setItemSalesSetting({
+              item_sales_status
+            })
+          }
         },
         {
           label: '发票设置',
           key: 'invoice_status',
           type: 'switch',
-          tip: '结算页是否显示发票'
+          tip: '结算页是否显示发票',
+          isShow: !VERSION_IN_PURCHASE,
+          onChange: async (e) => {
+            const { invoice_status } = this.form
+            await this.$api.company.setInvoiceStatus({
+              invoice_status
+            })
+          }
         },
         {
           label: '店务设置',
           key: 'dianwu_show_status',
           type: 'switch',
-          tip: '移动端会员中心是否展示店务端入口'
+          tip: '移动端会员中心是否展示店务端入口',
+          onChange: async (e) => {
+            const { dianwu_show_status } = this.form
+            await this.$api.company.setDianwuShowStatus({
+              dianwu_show_status
+            })
+          }
         },
         {
           label: '商品价格展示',
@@ -137,7 +218,10 @@ export default {
             { label: 1, name: '原价' },
             { label: 2, name: '会员等级价' },
             { label: 3, name: 'SVIP价' }
-          ]
+          ],
+          onChange: async (e) => {
+            this.saveItemPriceSetting()
+          }
         },
         {
           label: '购物车',
@@ -146,7 +230,10 @@ export default {
           options: [
             { label: 0, name: '销售价/会员等级价/SVIP价', disabled: true },
             { label: 1, name: '原价' }
-          ]
+          ],
+          onChange: async (e) => {
+            this.saveItemPriceSetting()
+          }
         },
         {
           label: '订单详情',
@@ -155,13 +242,19 @@ export default {
           options: [
             { label: 0, name: '销售价/会员等级价/SVIP价', disabled: true },
             { label: 1, name: '原价' }
-          ]
+          ],
+          onChange: async (e) => {
+            this.saveItemPriceSetting()
+          }
         }
       ]
     }
   },
   created () {
     this.fetch()
+  },
+  computed: {
+    ...mapGetters(['isMicorMall'])
   },
   methods: {
     async fetch () {
@@ -174,7 +267,7 @@ export default {
         minus_shop_gift_store: res.gift_setting.minus_shop_gift_store,
         check_gift_store: res.gift_setting.check_gift_store,
         ziti_send_oms: res.sendoms_setting.ziti_send_oms,
-        nostores_status: res.nostores_setting.nostores_status,
+        nostores_status: !res.nostores_setting.nostores_status,
         recharge_status: res.recharge_setting.recharge_status,
         repeat_cancel: res.cancel_setting.repeat_cancel,
         item_store_status: res.item_store_setting.item_store_status,
@@ -199,6 +292,26 @@ export default {
       if (item_page.svip_price) {
         this.form.item_page.push(3)
       }
+    },
+    async saveItemPriceSetting () {
+      const { cart_page, order_page, item_page } = this.form
+      const params = {
+        cart_page: {
+          price: true,
+          market_price: cart_page.indexOf(1) > -1
+        },
+        order_page: {
+          price: true,
+          market_price: order_page.indexOf(1) > -1
+        },
+        item_page: {
+          price: true,
+          market_price: item_page.indexOf(1) > -1,
+          member_price: item_page.indexOf(2) > -1,
+          svip_price: item_page.indexOf(3) > -1
+        }
+      }
+      await this.$api.company.saveItemPriceSetting(params)
     }
   }
 }

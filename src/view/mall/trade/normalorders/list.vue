14ab3ab194aa9a0d689fd730400834d0ae79da07
lukaijie <lukaijie@shopex.cn>
2022-09-07 17:28:04 +0800

    fix: 订单改价

--- src/view/mall/trade/normalorders/list.vue
+++ src/view/mall/trade/normalorders/list.vue
@@ -734,9 +734,11 @@ export default {
           component: () => (
             <CompTableView
               value={this.changePriceForm.items}
-              itemFee={this.changePriceForm.itemFee}
+              orderId={this.changePriceForm.order_id}
+              // itemFee={this.changePriceForm.itemFee}
               freightFee={this.changePriceForm.freightFee}
               orderFee={this.changePriceForm.orderFee}
+              on-onChange={this.onChangeTableView}
             />
           )
         }
@@ -750,8 +752,8 @@ export default {
         receive_address: '',
         loading: false,
         items: [],
-        // 商品应付金额
-        itemFee: 0,
+        // // 商品应付金额
+        // itemFee: 0,
         // 邮费
         freightFee: 0,
         // 订单应付金额
@@ -1068,7 +1070,7 @@ export default {
           this.changePriceForm.receive_mobile = receiver_mobile
           this.changePriceForm.receive_address = `${receiver_state}${receiver_city}${receiver_district}${receiver_address}`
         }
-        this.changePriceForm.itemFee = item_fee_new / 100
+        // this.changePriceForm.itemFee = item_fee_new / 100
         this.changePriceForm.freightFee = freight_fee / 100
         this.changePriceForm.orderFee = total_fee / 100
 
@@ -1076,8 +1078,7 @@ export default {
           return {
             ...item,
             change_discount: '',
-            change_price: '',
-            total: total_fee
+            change_price: ''
           }
         })
         console.log('this.changePriceForm.items:', this.changePriceForm.items)
@@ -1146,7 +1147,27 @@ export default {
       }
       this.refundDialog = false
     },
-    changePriceSubmit() {},
+    async changePriceSubmit() {
+      const { items, freightFee, order_id } = this.changePriceForm
+      let params = {
+        order_id,
+        down_type: 'items'
+      }
+      if (freightFee) {
+        params['freight_fee'] = freightFee
+      }
+      if (items.length > 0) {
+        params['items'] = items.map((item) => {
+          return {
+            item_id: item.item_id,
+            total_fee: item.total_fee
+          }
+        })
+      }
+      await this.$api.trade.changePriceConfirm(params)
+      this.changePriceDialog = false
+      this.fetchList()
+    },
     exportInvoice() {
       let type = 'normal'
       this.$emit('onChangeData', 'params', { type })
@@ -1239,6 +1260,19 @@ export default {
         placeholder: '请输入提货码',
         required: true,
         message: '不能为空'
+      })
+    },
+    onChangeTableView({ items, item_fee_new, freight_fee, total_fee }) {
+      // this.changePriceForm.itemFee = item_fee_new / 100
+      this.changePriceForm.freightFee = freight_fee / 100
+      this.changePriceForm.orderFee = total_fee / 100
+
+      this.changePriceForm.items = items.map((item) => {
+        return {
+          ...item,
+          change_discount: '',
+          change_price: ''
+        }
       })
     }
   }

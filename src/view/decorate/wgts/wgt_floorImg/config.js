ab0325c28c3e247aa51160097c5bc87bd074cd12
xiezhigang <xiezhigang@shopex.cn>
2023-02-07 17:56:28 +0800

    楼岑图片

---
+++ src/view/decorate/wgts/wgt_floorImg/config.js
@@ -0,0 +1,37 @@
+import AttrHotSetting from './attr-hotsetting'
+
+export default {
+  name: 'floorImg',
+  setting: [
+    { label: '标题', key: 'title', component: 'input', value: '标题' },
+    { label: '副标题', key: 'subtitle', component: 'input', value: '副标题' },
+    { label: '组件间距', key: 'padded', component: 'switch', value: false },
+    {
+      label: '热区设置',
+      key: 'data',
+      component: function (h, { key }) {
+        return <AttrHotSetting v-model={this.value[key]} />
+      },
+      value: { imgUrl: '', data: [] }
+    }
+  ],
+  transformIn: (v) => {
+    const {
+      name,
+      base,
+      config: { imgUrl },
+      data
+    } = v
+    return {
+      name,
+      ...base,
+      data: {
+        imgUrl,
+        data
+      }
+    }
+  },
+  transformOut: (v) => {
+    return v
+  }
+}
---
+++ src/view/decorate/wgts/wgt_floorImg/index.vue
@@ -0,0 +1,78 @@
+<style lang="scss">
+.wgt-floorImg {
+  &.padded {
+    padding: 10px 0;
+  }
+  .wgt-hd {
+    padding: 10px;
+    .title {
+      font-size: 18px;
+      font-weight: 600;
+      color: #333;
+    }
+    .sub-title {
+      color: #666;
+      margin-left: 4px;
+    }
+  }
+  .wgt-bd {
+    position: relative;
+    &.spaced {
+      padding: 0 10px;
+      .slider-container {
+        right: 10px;
+        left: 10px;
+      }
+      .slider-pagination {
+        right: 10px;
+        left: 10px;
+      }
+    }
+  }
+}
+</style>
+<template>
+  <div
+    :class="{
+      'wgt-floorImg': true,
+      'padded': value.padded
+    }"
+  >
+    <div v-if="value.title || value.subtitle" class="wgt-hd">
+      <span class="title">{{ value.title }}</span>
+      <span class="sub-title">{{ value.subtitle }}</span>
+    </div>
+    <div
+      class="wgt-bd"
+      :class="{
+        'spaced': value.spaced
+      }"
+    >
+      <el-image v-if="value.data.imgUrl" :src="value.data.imgUrl" :width="'100%'" />
+    </div>
+  </div>
+</template>
+
+<script>
+import config from './config'
+export default {
+  name: 'FloorImg',
+  wgtName: '热区图',
+  wgtDesc: '',
+  wgtIcon: 'wgt-floorImg',
+  config: config,
+  props: {
+    value: [Object, Array]
+  },
+  data() {
+    return {
+      currentIndex: 1
+    }
+  },
+  computed: {
+    sliderHeight() {}
+  },
+  created() {},
+  methods: {}
+}
+</script>

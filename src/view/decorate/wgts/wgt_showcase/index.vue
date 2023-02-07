1aea8a6b9e210c5e7623bab8f247b7ec4e7c91ba
lukaijie <lukaijie@shopex.cn>
2023-02-07 19:15:42 +0800

    add

---
+++ src/view/decorate/wgts/wgt_showcase/index.vue
@@ -0,0 +1,41 @@
+<style lang="scss" src="./index.scss"></style>
+<template>
+  <div
+    :class="{
+      'wgt-showcase': true,
+      'padded': value.padded
+    }"
+  >
+    <div v-if="value.title || value.subtitle" class="wgt-hd">
+      <span class="title">{{ value.title }}</span>
+      <span class="sub-title">{{ value.subtitle }}</span>
+    </div>
+    <div
+      class="wgt-bd"
+      :class="{
+        'spaced': value.spaced
+      }"
+    >
+      Showcase
+    </div>
+  </div>
+</template>
+
+<script>
+import config from './config'
+export default {
+  name: 'Showcase',
+  wgtName: '橱窗',
+  wgtDesc: '',
+  wgtIcon: 'wgt-showcase',
+  config: config,
+  props: {
+    value: [Object, Array]
+  },
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>

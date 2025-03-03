c206990af874e714f01228f2c0f4a9211978ab17
zhangjing <zhangjing@.shopexcn>
2025-03-03 18:20:08 +0800

    ECX-6158

--- src/view/mall/storeshop/store-shop.standard.vue
+++ src/view/mall/storeshop/store-shop.standard.vue
@@ -326,7 +326,7 @@ export default {
           },
             {
              name: '来源供应商',
-             key: 'operator_name',
+             key: 'supplier_name',
              width: 100,
            },
            {

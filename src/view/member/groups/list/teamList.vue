6933796a83b42bbfa0d21bb56a69bc5b3609891c
lukaijie <lukaijie@shopex.cn>
2025-05-16 15:56:15 +0800

    fix: bug修复

--- src/view/member/groups/list/teamList.vue
+++ src/view/member/groups/list/teamList.vue
@@ -1,26 +1,14 @@
 <template>
   <div>
-    <el-table
-      v-loading="loading"
-      :data="groupsTeamList"
-      style="width: 100%"
-      border
-      height="580"
-    >
-      <el-table-column
-        prop="username"
-        label="团长"
-      />
+    <el-table v-loading="loading" :data="groupsTeamList" style="width: 100%" border height="580">
+      <el-table-column prop="username" label="团长" />
       <el-table-column label="时间">
         <template slot-scope="scope">
           {{ scope.row.begin_time | datetime('YYYY-MM-DD HH:mm:ss') }} -
           {{ scope.row.end_time | datetime('YYYY-MM-DD HH:mm:ss') }}
         </template>
       </el-table-column>
-      <el-table-column
-        label="拼团状态"
-        width="100"
-      >
+      <el-table-column label="拼团状态" width="100">
         <template slot-scope="scope">
           <span v-if="scope.row.team_status == 1">进行中</span>
           <span v-else-if="scope.row.team_status == 2">成功</span>
@@ -32,24 +20,13 @@
           {{ scope.row.join_person_num }}/{{ groupsActivity.person_num }}
         </template>
       </el-table-column>
-      <el-table-column
-        label="操作"
-        width="250"
-      >
+      <el-table-column label="操作" width="250">
         <template slot-scope="scope">
-          <el-button
-            size="mini"
-            @click="showTeamAction(scope.$index, scope.row)"
-          >
-            查看
-          </el-button>
+          <el-button size="mini" @click="showTeamAction(scope.$index, scope.row)"> 查看 </el-button>
         </template>
       </el-table-column>
     </el-table>
-    <div
-      v-if="total_count > params.pageSize"
-      class="content-center content-top-padded"
-    >
+    <div v-if="total_count > params.pageSize" class="content-center content-top-padded">
       <el-pagination
         layout="prev, pager, next"
         :current-page.sync="params.page"
@@ -66,7 +43,7 @@ import { getGroupsTeamList } from '../../../../api/promotions'
 
 export default {
   props: ['view'],
-  data () {
+  data() {
     return {
       loading: false,
       groups_id: null,
@@ -81,17 +58,17 @@ export default {
       }
     }
   },
-  mounted () {
+  mounted() {
     this.groups_id = this.$route.params.groups_id
     this.params.view = this.view
     this.getGroupsTeamList()
   },
   methods: {
-    handleCurrentChange (pageNum) {
+    handleCurrentChange(pageNum) {
       this.params.page = pageNum
       this.getGroupsTeamList()
     },
-    getGroupsTeamList () {
+    getGroupsTeamList() {
       this.loading = true
       getGroupsTeamList(this.groups_id, this.params)
         .then((response) => {
@@ -108,8 +85,8 @@ export default {
           })
         })
     },
-    showTeamAction (index, row) {
-      this.$router.push({ path: '/marketing/groupsindex/detail/' + row.team_id })
+    showTeamAction(index, row) {
+      this.$router.push({ path: '/marketing/fissionmarketing/groupsindex/detail/' + row.team_id })
     }
   }
 }

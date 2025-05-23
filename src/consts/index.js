5cd66c04461a6ee86e0275fe894333acff44e902
lukaijie <lukaijie@shopex.cn>
2025-05-23 15:19:24 +0800

    fix: 更改title

--- src/consts/index.js
+++ src/consts/index.js
@@ -242,7 +242,7 @@ export const FORM_COMP = {
 
 export const SYSTEM_CONFIG = {
   platform: { name: 'Powered By ShopeX', theme: '#CB060F' },
-  standard: { name: '商派Onex云店', theme: '#FF9D00' },
+  standard: { name: '新零售云店', theme: '#FF9D00' },
   b2c: { name: '官方商城', theme: '#157FE3' },
   in_purchase: { name: '商派Onex内购', theme: '#EC442E' }
 }

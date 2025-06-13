87695e963b0237f23afa3cb4a3e945740f05706f
chenxin <398275325@qq>
2025-06-13 13:37:24 +0800

    路由设置

--- src/router/routes/modules/setting.js
+++ src/router/routes/modules/setting.js
@@ -1,3 +1,43 @@
-const routes = []
+import { BasicLayout } from '@/layout/basic' // 主框架
+
+const routes = [
+  {
+    component: BasicLayout,
+    meta: {
+      aliasName: 'setting',
+      icon: 'shopping-bag',
+      keepAlive: true,
+      title: '设置'
+    },
+    name: 'setting',
+    path: '/setting',
+    children: [
+      {
+        name: `account`,
+        path: 'platform/account',
+        meta: {
+          aliasName: 'Platformstaff',
+          icon: 'icon-products',
+          title: '平台账号',
+          permissions: ['setting.staff.platformstaff']
+        },
+        component: () => import('@/view/base/account/normalstaff'),
+      },
+      {
+        name: `storeAcount`,
+        path: 'store/account',
+        meta: {
+          aliasName: 'merchantstaff',
+          icon: 'icon-products',
+          title: '店铺账号',
+          permissions: ['setting.staff.merchantstaff']
+        },
+        component: () => import('@/view/base/account/storestaff'),
+      },
+      
+    ]
+  }
+]
 
 export default routes
+

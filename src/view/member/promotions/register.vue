b6b0064b6dfc7164fad409f7ec708d41eed547c6
lukaijie <lukaijie@shopex.cn>
2026-04-17 22:21:26 +0800

    v4.5.0

--- src/view/member/promotions/register.vue
+++ src/view/member/promotions/register.vue
@@ -42,7 +42,11 @@
               <div v-if="hasRegisterJumpPath" class="link-content border px-2">
                 <span @click="handleGoodsChange()">{{ registerJumpPathSummary }}</span>
                 <span style="margin-left: 10px">
-                  <i style="color: #f56c6c" class="el-icon-delete" @click.stop="clear_pic_url" />
+                  <i
+                    style="color: #f56c6c"
+                    class="el-icon-delete"
+                    @click.stop="clear_pic_url"
+                  />
                 </span>
               </div>
               <div
